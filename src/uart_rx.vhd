4a93605fe94a65f9be1df9a93e8f179a5f01614a
jedelist <jamieedelist@gmail.com>
2026-07-16 13:43:26 -0400

    Separated 2 flop sync into its own process. Fixed corrupt bit to spec. Change comments

--- src/uart_rx.vhd
+++ src/uart_rx.vhd
@@ -29,8 +29,26 @@ architecture rtl of uart_rx is
     signal clk_cnt : natural range 0 to CLKS_PER_BIT - 1 := 0;
     signal byte : std_logic_vector(7 downto 0) := (others => '0');
     signal bit_ptr : natural range 0 to 7 := 0;
+    signal rx_meta : std_logic := '1';          -- Initialized to high since UART line IDLEs at high
+    signal rx_sync : std_logic := '1';
 begin
-    uart_rx : process(clk, rst)
+
+    rx_synchronizer : process (clk, rst)
+    begin
+        if (rst = '1') then
+            -- Reset to 1 since rx_serial is active low
+            rx_meta <= '1';
+            rx_sync <= '1';
+        else
+            if (rising_edge clk) then
+                -- 2-flop synchronizer to sample stable version of rx_serial
+                rx_meta <= rx_serial;
+                rx_sync <= rx_meta;
+            end if;
+        end if;
+    end process rx_synchronizer;
+
+    uart_rx_fsm : process(clk, rst)
     begin
         if (rst = '1') then
             pkt <= (others => '0');
@@ -42,10 +60,11 @@ begin
             pkt_valid <= '0';
             byte <= (others => '0');
         elsif (rising_edge(clk)) then
-            pkt_valid <= '0';                               -- pkt_valid remains low and pulses high in STOP
+            pkt_valid <= '0';            -- pkt_valid remains low and pulses high in STOP
+            corrupt <= '0';              -- If a transmission is corrupt, only pulse for one cycle of this signal
             case state is 
                 when IDLE => 
-                    if (rx_serial = '0') then
+                    if (rx_sync = '0') then
                         -- Detected potential START bit
                         busy <= '1';
                         state <= START;
@@ -58,7 +77,7 @@ begin
                     end if;
                 when START =>
                     if (clk_cnt = CLKS_PER_BIT / 2) then     -- Sample in the middle of bit
-                        if (rx_serial = '0') then
+                        if (rx_sync = '0') then
                             state <= DATA;
                             clk_cnt <= 0;
                             busy <= '1';
@@ -73,7 +92,7 @@ begin
                     end if;
                 when DATA =>
                     if (clk_cnt = CLKS_PER_BIT - 1) then
-                        byte(bit_ptr) <= rx_serial;
+                        byte(bit_ptr) <= rx_sync;
                         clk_cnt <= 0;
                         if (bit_ptr = 7) then
                             state <= STOP;
@@ -86,7 +105,7 @@ begin
                     end if;
                 when STOP =>
                     if (clk_cnt = CLKS_PER_BIT - 1) then
-                        if (rx_serial = '1') then
+                        if (rx_sync = '1') then
                             pkt <= byte;
                             byte <= (others => '0');
                             pkt_valid <= '1';
@@ -102,12 +121,12 @@ begin
                             busy <= '0';
                             clk_cnt <= 0;
                             bit_ptr <= 0;
-                            
+
                         end if;
                     else 
                         clk_cnt <= clk_cnt + 1;
                     end if;
             end case;
         end if;
-    end process uart_rx;
+    end process uart_rx_fsm;
 end architecture rtl;
